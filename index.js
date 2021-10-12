6cb461f22b6df54dbc6838ff9cb064181f592da1
relathyme <relathyme@protonmail.com>
2021-10-12 16:23:46 +0700

    forgor

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require("./config.json")
 const client = new Eris.Client(config.token)
 client.options.allowedMentions.replied_user = true
 
-let limit = 100
+let limit = config.limit
 let messages = []
 
 client.on("ready", () => {
