0670c2e9cc909e04e75e6b39a5e919d515ae7661
relathyme <62229157+relathyme@users.noreply.github.com>
2024-04-20 01:48:14 +0700

    import dev branch changes
    add eval, exec, uptime in status

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ client.options.allowedMentions.replied_user = true
 let limit = config.limit
 let messages = []
 
+function u(){
+    let uptime=require("os").uptime()
+    let m=Math.floor(uptime/60%60)
+    let h=Math.floor(uptime/60/60%24)
+    let d=Math.floor(uptime/60/60/24)
+    const name=`for ${d}d ${h}h ${m}m`
+    return client.editStatus("online", {name})
+}
+
 client.once("ready", () => {
+    u()
+    setInterval(u, 60000)
     console.log("I'm ready!")
 })
 .on("messageCreate", async message => {
@@ -38,6 +49,29 @@ client.once("ready", () => {
         }
         await client.createMessage(message.channel.id, {content: msg.content, embed: msg.embed,
             messageReference: {channelID: message.channel.id, guildID: message.channel.guild.id, messageID: message.id}}, file).catch(console.error)
+    }
+})
+.on("messageCreate", async message => {
+    if(message.author.id!=config.owner) return
+    if(message.content.split(" ")[0]==config.prefix+"eval"){
+        const code=message.content.split(" ").slice(1).join(" ")
+        const acode=`(async () => {${code}})()`
+        try{
+            let result=await eval(acode)
+            if(typeof result != "string") result=require("util").inspect(result)
+            await client.createMessage(message.channel.id, `\`\`\`js\n${result}\`\`\``)
+        }catch(error){
+            await client.createMessage(message.channel.id, `\`\`\`js\n${error}\`\`\``)
+        }
+    }
+    if(message.content.split(" ")[0]==config.prefix+"exec"){
+        const command=message.content.split(" ").slice(1).join(" ")
+        try{
+            let result=require("child_process").execSync(command).toString()
+            await client.createMessage(message.channel.id, `\`\`\`sh\n${result}\`\`\``)
+        }catch(error){
+            await client.createMessage(message.channel.id, `\`\`\`sh\n${error}\`\`\``)
+        }
     }
 })
 .on("error", console.error)
